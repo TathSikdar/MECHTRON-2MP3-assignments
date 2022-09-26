99dd16e814702c505a17ac0817081b06ae766d21
sikdart <sikdart@pascal.cas.mcmaster.ca>
2022-09-26 11:26:37 -0400

    Fixed repetition issue in Q3

--- A1/Q3/question3.c
+++ A1/Q3/question3.c
@@ -7,22 +7,10 @@ int multiples (int x, int y, int N);
 int multiples (int x, int y, int N){
     int i;
     int sum = 0;
-
-    for (i=1; i<=N; i++){
-        if (x*i<=N) {
-            sum += x*i;
-        }
-        else{
-            break;
-        }
-    }
     
     for (i=1; i<=N; i++){
-        if(y*i<=N) {
-            sum+= y*i;
-        }
-        else{
-            break;
+        if (i%x == 0 || i%y ==0){
+            sum += i;
         }
     }
 
@@ -36,10 +24,3 @@ int main () {
 
     return 0;
 }
-
-// x=2, y=3
-
-// 2*_=multiple of 2
-
-// 1,2,3,4,5,6,7,8,9,10
-// /,^,
