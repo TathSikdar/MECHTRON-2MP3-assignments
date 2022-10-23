91ed451578eea3c163636c332dd0cdc71854681e
sikdart <sikdart@pascal.cas.mcmaster.ca>
2022-10-23 18:06:15 -0400

    finished question 2 without seg fault in test case 3

--- A2/Q2/question2.c
+++ A2/Q2/question2.c
@@ -4,7 +4,25 @@
 
 int juggler(int n);
 
+int juggler(int n){
+    
+    float temp = n;
+    if (n%2){   //Odd
+        n = floor(powf(temp,1.5));
+    } else{     //Even
+        n = floor(powf(temp,0.5));
+    }
+    
+    if (n==1){
+        return 1;
+    } else{
+        return juggler(n)+1;
+    }
+}
+
 int main () {
+
+    printf("\n%d",juggler(10000));
     
     return 0;
 }
