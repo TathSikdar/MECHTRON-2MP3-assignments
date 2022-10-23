f852feb672bbd9728e1fb3608457d01abbe9955d
sikdart <sikdart@pascal.cas.mcmaster.ca>
2022-10-23 18:22:06 -0400

    finished and tested question 3

--- A2/Q3/question3.c
+++ A2/Q3/question3.c
@@ -5,7 +5,38 @@
 
 int bubblesort(int* x, int size);
 
+int bubblesort(int* x, int size){
+
+    int sortedList[size];
+    int i, j;
+    int counter = 0;
+
+    for(i=0; i<size-1; i++){
+        for(j=0; j<size-i-1; j++){
+            int compare1, compare2;
+            if(x[j]>x[j+1]){
+                compare1 = x[j];
+                compare2 = x[j+1];
+
+                x[j] = compare2;
+                x[j+1] = compare1;
+                
+                counter++;
+            }
+        }
+    }
+    return counter;
+}
+
 int main () {
+
+    int x[]={100};
+
+    int sizex = sizeof(x)/sizeof(x[0]);
+
+    printf("%d", bubblesort(x, sizex));
+
+
     
     return 0;
 }
